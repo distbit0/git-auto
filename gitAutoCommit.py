31532553386589234e71cf6e2f752240173ab50f
Dist Bit <adp01@pm.me>
2023-09-20 11:43:30 +1000

    gitAutoCommit.py

--- gitAutoCommit.py
+++ gitAutoCommit.py
@@ -10,8 +10,6 @@ def generate_commit_message():
     )
     commit_message = result.stdout.strip()
 
-    print("Commit message: " + commit_message)
-
     # Remove any paths that start with a dot or contain a slash followed by a dot
     commit_message = "\n".join(
         [
@@ -29,8 +27,6 @@ def generate_commit_message():
 
 
 def main():
-    custom_message = sys.argv[1] if len(sys.argv) > 1 else generate_commit_message()
-
     # Check if there are any changes in the working directory
     changes_in_index = subprocess.run(
         ["git", "diff-index", "--quiet", "HEAD", "--"], capture_output=True, text=True
@@ -47,6 +43,9 @@ def main():
 
     # Stage all changes
     subprocess.run(["git", "add", "."])
+
+    # Generate commit message if not provided
+    custom_message = sys.argv[1] if len(sys.argv) > 1 else generate_commit_message()
 
     # Commit the changes with the custom message
     commit_status = subprocess.run(["git", "commit", "-m", custom_message])
@@ -66,4 +65,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-#
