c70bd1cbe7b4a558489bdce3dd8e47bcb552c7e3
Dist Bit <adp01@pm.me>
2023-10-26 11:17:32 +1100

    gitAutoCommit.py git_auto_commit.log

--- gitAutoCommit.py
+++ gitAutoCommit.py
@@ -2,6 +2,11 @@ import subprocess
 import re
 import argparse
 import os
+import logging
+import time
+
+logging.basicConfig(filename="git_auto_commit.log", level=logging.INFO)
+
 
 def generate_commit_message():
     result = subprocess.run(
@@ -20,16 +25,41 @@ def generate_commit_message():
 
     return commit_message
 
+
+def check_git_process():
+    result = subprocess.run(["ps", "-ef"], capture_output=True, text=True)
+    return "git" in result.stdout
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", help="Path to apply the git operations to", default=".")
+    parser.add_argument(
+        "-p", "--path", help="Path to apply the git operations to", default="."
+    )
     parser.add_argument("-m", "--message", help="Custom commit message", default=None)
     args = parser.parse_args()
 
     if args.path != ".":
         os.chdir(args.path)
-    
-    subprocess.run(["git", "add", "."])
+
+    lock_file_path = os.path.join(args.path, ".git", "index.lock")
+
+    if os.path.exists(lock_file_path):
+        logging.warning(
+            f"{lock_file_path} exists in repo {args.path}. Waiting for other git operations to finish."
+        )
+        time.sleep(10)
+        if os.path.exists(lock_file_path):
+            if check_git_process():
+                logging.error(f"Git process running in repo {args.path}. Exiting.")
+                exit(1)
+            else:
+                logging.warning(
+                    f"Removing lock file in repo {args.path} after waiting."
+                )
+                os.remove(lock_file_path)
+
+    subprocess.run(["git", "add", "."], check=True)
 
     changes_in_index = subprocess.run(
         ["git", "diff-index", "--quiet", "HEAD", "--"], capture_output=True, text=True
@@ -41,22 +71,23 @@ def main():
     ).stdout.strip()
 
     if changes_in_index.returncode == 0 and not changes_not_staged:
-        print("No changes to commit.")
+        logging.info(f"No changes to commit in repo {args.path}.")
         return
 
     custom_message = args.message if args.message else generate_commit_message()
 
-    commit_status = subprocess.run(["git", "commit", "-m", custom_message])
+    commit_status = subprocess.run(["git", "commit", "-m", custom_message], check=True)
 
     if commit_status.returncode == 0:
-        print("Commit successful. Pushing to remote.")
-        push_status = subprocess.run(["git", "push"])
+        logging.info(f"Commit successful in repo {args.path}. Pushing to remote.")
+        push_status = subprocess.run(["git", "push"], check=True)
         if push_status.returncode == 0:
-            print("Push successful.")
+            logging.info(f"Push successful in repo {args.path}.")
         else:
-            print("Push failed.")
+            logging.error(f"Push failed in repo {args.path}.")
     else:
-        print("Commit failed.")
+        logging.error(f"Commit failed in repo {args.path}.")
+
 
 if __name__ == "__main__":
     main()
